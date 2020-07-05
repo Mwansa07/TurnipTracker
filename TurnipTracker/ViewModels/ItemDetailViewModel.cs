0fc6b33a73f94560e061c934d01a9d589c34bcdd
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-05 22:00:05 +0200

    * .gitignore: First Commit Turnip Tracker
    * TurnipTracker.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppDelegate.cs:
    * AppShell.xaml:
    * ItemsPage.xaml:
    * AboutPage.xaml:
    * TurnipTracker.csproj:
    * Entitlements.plist:
    * NewItemPage.xaml:
    * AppShell.xaml.cs:
    * IDataStore.cs:
    * AboutPage.xaml.cs:
    * ItemsPage.xaml.cs:
    * MainActivity.cs:
    * ItemDetailPage.xaml:
    * Default.png:
    * MockDataStore.cs:
    * NewItemPage.xaml.cs:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * ItemDetailPage.xaml.cs:
    * ItemsViewModel.cs:
    * AboutViewModel.cs:
    * TurnipTracker.iOS.csproj:
    * Default@2x.png:
    * tab_feed@3x.png:
    * tab_feed@2x.png:
    * tab_about@2x.png:
    * xamarin_logo.png:
    * tab_about@3x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * xamarin_logo@3x.png:
    * xamarin_logo@2x.png:
    * Default-568h@2x.png:
    * ItemDetailViewModel.cs:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * Tabbar.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * TurnipTracker.Android.csproj:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * tab_feed.png:
    * icon.png:
    * tab_about.png:
    * icon.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon76.png:
    * Icon60.png:
    * Icon20.png:
    * Icon80.png:
    * Icon29.png:
    * Icon40.png:
    * Icon58.png:
    * Icon87.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon152.png:
    * Icon167.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ TurnipTracker/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TurnipTracker.Models;
+
+namespace TurnipTracker.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
