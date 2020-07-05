af1d30b4fda2de066f0e665b1188ee32dd43a2e8
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-06 00:46:51 +0200

    Added Turnip Tracking page and Material configuration

---
+++ TurnipTracker/Views/TurnipTrackingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TurnipTracker.Views
+{
+    public partial class TurnipTrackingPage : ContentPage
+    {
+        public TurnipTrackingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
